b8802d3c807b18d3daf85f51ad0b02cc064bd3d1
jatin-yadav <jatinyadav19oct@gmail.com>
2023-11-26 10:59:18 +0530

    Elite Estate

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, Home, Profile, SignIn, SignUp } from "./pages";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import About from "./About";
+import Home from "./Home";
+import Profile from "./Profile";
+import SignUp from "./SignUp";
+import SignIn from "./Signin";
+
+export { About, Home, SignIn, SignUp, Profile };
