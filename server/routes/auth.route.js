7ae8b181c4afe8bb5828ce3e1de1f9acb453ce46
jatin-yadav <jatinyadav19oct@gmail.com>
2023-11-28 10:39:57 +0530

    signin api route

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { signUp } from "../controllers/auth.controller.js";
+import { signIn, signUp } from "../controllers/auth.controller.js";
 
 dotenv.config();
 
 const router = express.Router();
 
 router.route("/signup").post(signUp);
+router.route("/signin").post(signIn);
+
 
 export default router;
