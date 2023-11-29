0a548f0cc626ce137a208c8797a0599cf79d7e8b
jatin-yadav <jatinyadav19oct@gmail.com>
2023-11-29 16:41:07 +0530

    Update Profile Image

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,22 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import OAuth from "../components/OAuth";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
   const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.user);
-
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -45,20 +52,73 @@ export default function Profile() {
   };
 
   const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        console.log(error);
+        setFileUploadError(true);
+      },
+      () => {
+        setFileUploadError(false);
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
 
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input type="file" hidden accept="image/*" ref={fileRef} />
+        <input
+          type="file"
+          hidden
+          accept="image/*"
+          ref={fileRef}
+          onChange={(e) => setFile(e.target.files[0])}
+        />
         <img
           onClick={() => {
             fileRef.current.click();
           }}
-          src={currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt="avatar"
           className="self-center rounded-full h-24 w-24 object-cover cursor-pointer"
         />
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-700">
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className="text-green-700">Image successfully uploaded!</span>
+          ) : (
+            ""
+          )}
+        </p>
         <input
           type="text"
           placeholder="username"
@@ -87,7 +147,6 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
-        <OAuth />
       </form>
       <div className="flex gap-2 mt-5 justify-between">
         <Link to="/">
