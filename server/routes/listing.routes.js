215ef416f83178c01a8bfc20b78e6408621c437e
jatin-yadav <jatinyadav19oct@gmail.com>
2023-12-01 16:37:00 +0530

    header search form

--- server/routes/listing.routes.js
+++ server/routes/listing.routes.js
@@ -6,6 +6,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -18,5 +19,6 @@ router.route("/create").post(verifyToken, createListing);
 router.route("/delete/:id").delete(verifyToken, deleteListing);
 router.route("/update/:id").post(verifyToken, updateListing);
 router.route("/get/:id").get(getListing);
+router.route("/get").get(getListings);
 
 export default router;
